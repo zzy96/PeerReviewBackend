0b7b78d2ac092f414ad6f134f147481bae0f90e5
zzy96 <zhouzhiyaohflser@gmail.com>
2017-12-22 16:49:29 +0800

    allow cors

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', index);
 
