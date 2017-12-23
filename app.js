96ecb197af428b9309a48f01f2de46ee46c23e11
zzy96 <zhouzhiyaohflser@gmail.com>
2017-12-23 12:20:32 +0800

    add static file service

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
